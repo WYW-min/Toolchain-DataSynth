01e905fa9eabb1076910a26c25830c9b5b08b7d1
wyw <ywwang@feishudata.com>
2026-01-16 10:25:31 +0800

    dev/wyw: 修改了git-push描述

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -1,14 +1,14 @@
-git checkout dev/wyw   # 切到自己的个人分支（永远是这一步开头）
+git checkout <自己的分支名>   # 切到自己的个人分支（永远是这一步开头）
 git checkout main      # 切到main中转站
 git push --set-upstream origin main
 
 
-git checkout dev/wyw
+# 本地main分支永远只执行这两个命令
+git checkout <自己的分支名>
 git merge main
 
 git add .
-git commit -m "dev/wyw：完成xxx开发/修复xxx问题"
-
-
+git commit -m "<自己的分支名>：完成xxx开发/修复xxx问题"
+git push
 
 echo "Done."
